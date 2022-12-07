c32b998bb4d6b08c3a7afcd72626d8138ac29dc3
dzdidi <deniszalessky@gmail.com>
2022-12-07 10:06:34 +0100

    lint jest
    Signed-off-by: dzdidi <deniszalessky@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,13 @@ module.exports = {
     browser: true,
     commonjs: true,
     es2021: true,
+    'jest/globals': true
   },
   extends: 'airbnb-base',
   overrides: [
+  ],
+  plugins: [
+    'jest'
   ],
   parserOptions: {
     ecmaVersion: 'latest',
