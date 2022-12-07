b878273a35683be4dbea8aba40aa8b435aa49e17
dzdidi <deniszalessky@gmail.com>
2022-12-07 10:24:46 +0100

    todos; and order cancelling boilerplate
    Signed-off-by: dzdidi <deniszalessky@gmail.com>

--- src/OrderBook.js
+++ src/OrderBook.js
@@ -24,6 +24,7 @@ module.exports = class OrderBook {
     this.grape.on('ready', () => { this.bootstraped = true; });
 
     this.order = null;
+    this.orderInProgress = false
   }
 
   async waitUntilBootstraped(seconds = 5) {
@@ -40,7 +41,7 @@ module.exports = class OrderBook {
   async waitUntilOrderProcessed(seconds = 5) {
     let remaining = seconds;
     while (remaining > 0) {
-      if (!this.order) return;
+      if (!this.orderInProgress) return;
 
       // eslint-disable-next-line
       await setTimeout(1000);
@@ -56,12 +57,31 @@ module.exports = class OrderBook {
     this.grape.stop();
   }
 
+  async cancelOrder() {
+    // TODO:
+    // stop anouncing
+    //
+    // wait untill processed
+  }
+
   submitOrder() {
+    this.orderInProgress = true;
+    // TODO: lookup if not found start anouncing
+    // if found and filled completely - good
+    // if found and filled partially - call itself
+
+
     // TODO: move to constructor OR do this only if lookup failed
     // this.orderService = this.server.transport('server');
     // this.orderService.listen(this.config.service_port);
     // this.orderService.on('request', this.matchOrder);
+    this.orderInProgress = false;
   }
 
-  matchOrder() {}
+  matchOrder() {
+    this.orderInProgress = true;
+    // TODO: if filled completely - stop anouncing
+    // if filled partially subtract
+    this.orderInProgress = false;
+  }
 };
